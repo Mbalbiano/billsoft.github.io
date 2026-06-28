32c9468610134cf0a2bf5089a87623703c7dfb7f
Valentino Ekes <vekes@uade.edu.ar>
2026-06-28 17:02:43 -0300

    Paginas principales hechas falta un mejor diseño

--- assets/JS/Validacion.js
+++ assets/JS/Validacion.js
@@ -1,10 +1,64 @@
-//Names que necesito NombreCompra, Categoria, PrecioCompra
-const formulario=document.querySelector('form')
-formulario.addEventListener('submit', function(event){
-const datos=new FormData(formulario)
-var NombreCompra=datos.get('NombreCompra')
-var Categoria=datos.get('Categoria')
-var PrecioCompra=datos.get('PrecioCompra')
-var mensaje=`La compra que hizo fue ${NombreCompra} la categoria fue ${Categoria} y gasto ${PrecioCompra} pesos`
-alert(mensaje)
-})
+const formulario = document.querySelector(".formulario");
+
+if (formulario) {
+    formulario.addEventListener("submit", function(event){
+        event.preventDefault();
+
+        const datos = new FormData(formulario);
+
+        let NombreCompra = datos.get("NombreCompra");
+        let Categoria = datos.get("Categoria");
+        let PrecioCompra = datos.get("PrecioCompra");
+
+        let mensaje = `La compra que hizo fue ${NombreCompra}, la categoría fue ${Categoria} y gastó ${PrecioCompra} pesos`;
+
+        alert(mensaje);
+    });
+}
+
+const formulario2 = document.querySelector(".formulario2");
+
+if (formulario2) {
+    formulario2.addEventListener("submit", function(event){
+        event.preventDefault();
+
+        const datos2 = new FormData(formulario2);
+
+        let nombre = datos2.get("Nombre");
+        let apellido = datos2.get("Apellido");
+        let email = datos2.get("Email");
+        let contraseña = datos2.get("Contraseña");
+        let confirmacion = datos2.get("Confirmacion");
+        const mensaje="Registro exitoso"
+    if (nombre==""|| apellido==""|| email==""|| contraseña=="" || confirmacion==""){
+        alert("Tiene que completar todos los campos")
+    
+    }
+    else if(contraseña!==confirmacion){
+        alert("Las contraseñas no coinciden")
+    }
+    else{
+        alert(mensaje)
+        formulario2.reset()
+        window.location.href="../seccion/IngresoGasto.html"
+    }
+    });
+}
+const formulario3=document.querySelector('.formulario3')
+if (formulario3){
+    formulario3.addEventListener("submit", function(event){
+        event.preventDefault();
+        const datos3= new FormData(formulario3)
+        let email2=datos3.get('Email2')
+        let contraseña2=datos3.get('Contraseña2')
+        const mensaje=`Usuario logueado`
+        if(email2===""||contraseña2===""){
+            alert("Complete los campos")
+        }
+        else{
+            alert(mensaje)
+            formulario3.reset()
+            window.location.href="../seccion/IngresoGasto.html"
+        }
+    })
+}
